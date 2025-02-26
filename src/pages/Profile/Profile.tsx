f527d7becef07db75c9351c52eefa76e864cfe9b
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-02-26 21:26:02 +0200

    add OrdersSymbol, Profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,23 +2,45 @@ import React from 'react';
 import './Profile.scss';
 import { FundsSymbol } from '../../components/FundsSymbol';
 import { OrdersSymbol } from '../../components/OrdersSymbol';
+import { useDispatch } from 'react-redux';
+import {
+    decreaseFunds,
+    increaseFunds,
+    clearFunds,
+} from '../../store/funds/funds-actions';
+import { clearOrders } from '../../store/orders/orders-actions';
 
 export const Profile: React.FC = () => {
+    const fundsStep = 1000;
+    const dispatch = useDispatch();
+    const handleDecreaseFunds = () => {
+        dispatch(decreaseFunds(fundsStep));
+    };
+    const handleIncreaseFunds = () => {
+        dispatch(increaseFunds(fundsStep));
+    };
+    const handleClearFunds = () => {
+        dispatch(clearFunds());
+    };
+    const handleClearOrders = () => {
+        dispatch(clearOrders());
+    };
+
     return (
         <div className="Profile">
             <div>Profile</div>
             <div>for debug:</div>
             <div className="Profile__funds">
-                <button>- 1000</button>
+                <button onClick={handleDecreaseFunds}>- {fundsStep}</button>
                 <FundsSymbol />
 
-                <button>+ 1000</button>
-                <button>clear</button>
+                <button onClick={handleIncreaseFunds}>+ {fundsStep}</button>
+                <button onClick={handleClearFunds}>clear</button>
             </div>
 
             <div className="Profile__orders">
                 <OrdersSymbol />
-                <button>clear</button>
+                <button onClick={handleClearOrders}>clear</button>
             </div>
         </div>
     );
