c3ab1979707464cadb77bf44bd9ea051edb20ef5
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2026-06-14 08:52:29 +0300

    change API URL and key

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-const API_KEY = 'b20dacc5-49ad3ada-c8620c10-fe201b28';
+const API_KEY = 'd98c7fa1-e622-49c7-b929-fbae7b6684b7';
 
-const ALL_GOODS = 'https://fortniteapi.io/v2/shop?lang=en';
+const ALL_GOODS = 'https://fortnite-api.com/v2/shop?lang=en';
 
 export { API_KEY, ALL_GOODS };
